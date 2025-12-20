894d2e1661cfc0d24bb6790ba2e683fe37d75494
Majibrin <muhammadabdullahijibrin0@gmail.com>
2025-12-20 18:28:40 +0100

    Edit API_BASE in authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,5 +1,5 @@
 // authService.js - SIMPLIFIED VERSION
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export const authService = {
     // Login with username/password
